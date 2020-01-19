d669c0e58447a480c4345407627525559b0ec330
Pierre-Yves Gillier <pierre-yves@gillier.name>
2020-01-19 21:14:59 +0100

    [API] Move to relay

--- feeds/schema.py
+++ feeds/schema.py
@@ -1,25 +1,48 @@
 import graphene
-from graphene_django.types import DjangoObjectType
-from feeds.models import Feed, Episode
+from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
+
+from feeds.models import Feed, Episode, Enclosure
 
 
-class FeedType(DjangoObjectType):
+# Relay
+class FeedNode(DjangoObjectType):
     class Meta:
         model = Feed
+        exclude = ('created_at', 'updated_at')
+        filter_fields = ()
+        interfaces = (graphene.relay.Node, )
+
+    # Returns full URL from storage
+    def resolve_cover(self, info):
+        return self.cover.url
 
 
-class EpisodeType(DjangoObjectType):
+class EpisodeNode(DjangoObjectType):
     class Meta:
         model = Episode
+        exclude = ('created_at', 'updated_at')
+        filter_fields = ('feed',)
+        interfaces = (graphene.relay.Node, )
 
 
-class Query(object):
-    all_feeds = graphene.List(FeedType)
-    all_episodes = graphene.List(EpisodeType)
+class EnclosureNode(DjangoObjectType):
+    class Meta:
+        model = Enclosure
+        exclude = ('created_at', 'updated_at')
+        filter_fields = ('episode',)
+        interfaces = (graphene.relay.Node, )
 
-    def resolve_all_feeds(self, info, **kwargs):
-        return Feed.objects.all()
+    def resolve_content(self, info):
+        return self.content.url
 
-    def resolve_all_episodes(self, info, **kwargs):
-        # We can easily optimize query count in the resolve method
-        return Episode.objects.select_related('feed').all()
+
+class Query(graphene.ObjectType):
+    feed = graphene.relay.Node.Field(FeedNode)
+    all_feeds = DjangoFilterConnectionField(FeedNode)
+
+    episode = graphene.relay.Node.Field(EpisodeNode)
+    all_episodes = DjangoFilterConnectionField(EpisodeNode)
+
+    enclosure = graphene.relay.Node.Field(EnclosureNode)
+    all_enclosures = DjangoFilterConnectionField(EnclosureNode)
